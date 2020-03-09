ce04080267d2d350102278b479b7e6fa9d454a65
Lyn <liyuning.tan@qq.com>
2020-03-09 17:09:04 +0800

    取消功能实现

--- src/core/dispatchRequest.ts
+++ src/core/dispatchRequest.ts
@@ -5,6 +5,7 @@ import { flattenHeaders } from '../helpers/headers'
 import transform from './transform'
 
 export default function dispatchRequest(config: AxiosRequestConfig): AxiosPromise {
+  throwIfCancellationRequested(config)
   // 预处理后的config
   processConfig(config)
   // 发起请求
@@ -30,4 +31,10 @@ function transformURL(config: AxiosRequestConfig): string {
 function transformResponseData(res: AxiosResponse): AxiosResponse {
   res.data = transform(res.data, res.headers, res.config.transformResponse)
   return res
+}
+
+function throwIfCancellationRequested(config: AxiosRequestConfig): void {
+  if (config.cancelToken) {
+    config.cancelToken.throwIfRequested()
+  }
 }
